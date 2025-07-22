fb7fa059a4c68ebb3883697031299381feedf391
haoruiii <93047603+haoruiii@users.noreply.github.com>
2025-07-22 06:09:39 +0000

    Deploying to gh-pages from @ haoruiii/haoruiii.github.io@d4cd415130057007b7a90b8542cccbdb62133ad9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,39 +501,25 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/en-us/6_project/";
             },},{
+        id: 'social-bluesky',
+        title: 'Bluesky',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://bsky.app/profile/haoruiii.bsky.social", "_blank");
+        },
+      },{
         id: 'social-email',
         title: 'Send an email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%68%61%6F%72%75%69%69%69@%75%6D%69%63%68.%65%64%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.linkedin.com/in/haoruiii", "_blank");
         },
       },{
           id: 'lang-zh-cn',
