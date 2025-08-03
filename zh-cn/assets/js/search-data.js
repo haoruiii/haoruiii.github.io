9436463d0f7f564d715c3a9346e2c7557b577927
haoruiii <93047603+haoruiii@users.noreply.github.com>
2025-08-03 05:24:40 +0000

    Deploying to gh-pages from @ haoruiii/haoruiii.github.io@b3d8118e4cb945c475c26c9b0f58011cbe30488d 🚀

--- zh-cn/assets/js/search-data.js
+++ zh-cn/assets/js/search-data.js
@@ -69,257 +69,15 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/zh-cn/news/";
               },
-            },{id: "post-a-post-with-plotly-js",
-        
-          title: "a post with plotly.js",
-        
-        description: "this is what included plotly.js code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2025/plotly/";
-          
-        },
-      },{id: "post-a-post-with-image-galleries",
-        
-          title: "a post with image galleries",
-        
-        description: "this is what included image galleries could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2024/photo-gallery/";
-          
-        },
-      },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
+            },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
         
           title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
         
         description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-        section: "Posts",
+        section: "博客",
         handler: () => {
           
             window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-          
-        },
-      },{id: "post-a-post-with-tabs",
-        
-          title: "a post with tabs",
-        
-        description: "this is what included tabs in a post could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2024/tabs/";
-          
-        },
-      },{id: "post-a-post-with-typograms",
-        
-          title: "a post with typograms",
-        
-        description: "this is what included typograms code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2024/typograms/";
-          
-        },
-      },{id: "post-a-post-that-can-be-cited",
-        
-          title: "a post that can be cited",
-        
-        description: "this is what a post that can be cited looks like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2024/post-citation/";
-          
-        },
-      },{id: "post-a-post-with-pseudo-code",
-        
-          title: "a post with pseudo code",
-        
-        description: "this is what included pseudo code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2024/pseudocode/";
-          
-        },
-      },{id: "post-a-post-with-code-diff",
-        
-          title: "a post with code diff",
-        
-        description: "this is how you can display code diffs",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2024/code-diff/";
-          
-        },
-      },{id: "post-a-post-with-advanced-image-components",
-        
-          title: "a post with advanced image components",
-        
-        description: "this is what advanced image components could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2024/advanced-images/";
-          
-        },
-      },{id: "post-a-post-with-vega-lite",
-        
-          title: "a post with vega lite",
-        
-        description: "this is what included vega lite code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2024/vega-lite/";
-          
-        },
-      },{id: "post-a-post-with-geojson",
-        
-          title: "a post with geojson",
-        
-        description: "this is what included geojson code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2024/geojson-map/";
-          
-        },
-      },{id: "post-a-post-with-echarts",
-        
-          title: "a post with echarts",
-        
-        description: "this is what included echarts code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2024/echarts/";
-          
-        },
-      },{id: "post-a-post-with-chart-js",
-        
-          title: "a post with chart.js",
-        
-        description: "this is what included chart.js code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2024/chartjs/";
-          
-        },
-      },{id: "post-a-post-with-tikzjax",
-        
-          title: "a post with TikZJax",
-        
-        description: "this is what included TikZ code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2023/tikzjax/";
-          
-        },
-      },{id: "post-a-post-with-bibliography",
-        
-          title: "a post with bibliography",
-        
-        description: "an example of a blog post with bibliography",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2023/post-bibliography/";
-          
-        },
-      },{id: "post-a-post-with-jupyter-notebook",
-        
-          title: "a post with jupyter notebook",
-        
-        description: "an example of a blog post with jupyter notebook",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2023/jupyter-notebook/";
-          
-        },
-      },{id: "post-a-post-with-custom-blockquotes",
-        
-          title: "a post with custom blockquotes",
-        
-        description: "an example of a blog post with custom blockquotes",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2023/custom-blockquotes/";
-          
-        },
-      },{id: "post-a-post-with-table-of-contents-on-a-sidebar",
-        
-          title: "a post with table of contents on a sidebar",
-        
-        description: "an example of a blog post with table of contents on a sidebar",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2023/sidebar-table-of-contents/";
-          
-        },
-      },{id: "post-a-post-with-audios",
-        
-          title: "a post with audios",
-        
-        description: "this is what included audios could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2023/audios/";
-          
-        },
-      },{id: "post-a-post-with-videos",
-        
-          title: "a post with videos",
-        
-        description: "this is what included videos could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2023/videos/";
-          
-        },
-      },{id: "post-displaying-beautiful-tables-with-bootstrap-tables",
-        
-          title: "displaying beautiful tables with Bootstrap Tables",
-        
-        description: "an example of how to use Bootstrap Tables",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2023/tables/";
-          
-        },
-      },{id: "post-a-post-with-table-of-contents",
-        
-          title: "a post with table of contents",
-        
-        description: "an example of a blog post with table of contents",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2023/table-of-contents/";
-          
-        },
-      },{id: "post-a-post-with-giscus-comments",
-        
-          title: "a post with giscus comments",
-        
-        description: "an example of a blog post with giscus comments",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2022/giscus-comments/";
           
         },
       },{id: "post-displaying-external-posts-on-your-al-folio-blog",
@@ -327,109 +85,10 @@ ninja.data = [{
           title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
         
         description: "",
-        section: "Posts",
+        section: "博客",
         handler: () => {
           
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-          
-        },
-      },{id: "post-a-post-with-redirect",
-        
-          title: "a post with redirect",
-        
-        description: "you can also redirect to assets like pdf",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/assets/pdf/pt-br/example_pdf.pdf";
-          
-        },
-      },{id: "post-a-post-with-diagrams",
-        
-          title: "a post with diagrams",
-        
-        description: "an example of a blog post with diagrams",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2021/diagrams/";
-          
-        },
-      },{id: "post-a-post-with-twitter",
-        
-          title: "a post with twitter",
-        
-        description: "an example of a blog post with twitter",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2020/twitter/";
-          
-        },
-      },{id: "post-a-distill-style-blog-post",
-        
-          title: "a distill-style blog post",
-        
-        description: "an example of a distill-style blog post and main elements",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2018/distill/";
-          
-        },
-      },{id: "post-a-post-with-disqus-comments",
-        
-          title: "a post with disqus comments",
-        
-        description: "an example of a blog post with disqus comments",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2015/disqus-comments/";
-          
-        },
-      },{id: "post-a-post-with-math",
-        
-          title: "a post with math",
-        
-        description: "an example of a blog post with some math",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2015/math/";
-          
-        },
-      },{id: "post-uma-postagem-com-código",
-        
-          title: "uma postagem com código",
-        
-        description: "um exemplo de uma postagem em um blog com código",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2015/code/";
-          
-        },
-      },{id: "post-a-post-with-images",
-        
-          title: "a post with images",
-        
-        description: "this is what included images could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2015/images/";
-          
-        },
-      },{id: "post-a-post-with-formatting-and-links",
-        
-          title: "a post with formatting and links",
-        
-        description: "march &amp; april, looking forward to summer",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/zh-cn/blog/2015/formatting-and-links/";
           
         },
       },{id: "books-the-godfather",
@@ -457,84 +116,73 @@ ninja.data = [{
           description: "",
           section: "",handler: () => {
               window.location.href = "/zh-cn/books/zh-cn/the_godfather/";
-            },},{id: "news-um-anúncio-simples-em-uma-linha",
-          title: 'Um anúncio simples em uma linha.',
-          description: "",
-          section: "简讯",},{id: "news-um-anúncio-longo-com-detalhes",
-          title: 'Um anúncio longo com detalhes',
-          description: "",
-          section: "简讯",handler: () => {
-              window.location.href = "/zh-cn/news/zh-cn/announcement_2/";
-            },},{id: "news-um-anúncio-simples-em-uma-linha-com-markdown-emoji-sparkles-smile",
-          title: 'Um anúncio simples em uma linha com Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "简讯",},{id: "projects-试论当代中国的教育与流动性变迁",
+            },},{id: "projects-试论当代中国的教育与流动性变迁",
           title: '试论当代中国的教育与流动性变迁',
           description: "来自CGSS的一些结果",
-          section: "Projects",handler: () => {
+          section: "课题",handler: () => {
               window.location.href = "/zh-cn/projects/zh-cn/project_1/";
             },},{id: "projects-当代中国的教育流动性及其变化",
           title: '当代中国的教育流动性及其变化',
           description: "关于年龄组、性别、户口和地域",
-          section: "Projects",handler: () => {
+          section: "课题",handler: () => {
               window.location.href = "/zh-cn/projects/zh-cn/project_2/";
             },},{id: "projects-本科期间的其他研究经历",
           title: '本科期间的其他研究经历',
           description: "",
-          section: "Projects",handler: () => {
+          section: "课题",handler: () => {
               window.location.href = "/zh-cn/projects/zh-cn/project_3/";
             },},{id: "projects-城村烟火",
           title: '城村烟火',
           description: "记2023深圳高校联合工作坊",
-          section: "Projects",handler: () => {
+          section: "课题",handler: () => {
               window.location.href = "/zh-cn/projects/zh-cn/project_4/";
             },},{id: "projects-画笔下的回忆",
           title: '画笔下的回忆',
           description: "南京市科利华中学原创心理剧",
-          section: "Projects",handler: () => {
+          section: "课题",handler: () => {
               window.location.href = "/zh-cn/projects/zh-cn/project_5/";
             },},{id: "projects-致青春",
           title: '致青春',
           description: "NFLS 2021届高三留念视频",
-          section: "Projects",handler: () => {
+          section: "课题",handler: () => {
               window.location.href = "/zh-cn/projects/zh-cn/project_6/";
             },},{id: "projects-再见",
           title: '再见',
           description: "NFLS 2021届高三毕业视频",
-          section: "Projects",handler: () => {
+          section: "课题",handler: () => {
               window.location.href = "/zh-cn/projects/zh-cn/project_7/";
             },},{
         id: 'social-email',
         title: '发邮件',
-        section: 'Socials',
+        section: '社交媒体',
         handler: () => {
           window.open("mailto:%68%61%6F%72%75%69%69%69@%75%6D%69%63%68.%65%64%75", "_blank");
         },
       },{
         id: 'social-bluesky',
         title: 'Bluesky',
-        section: 'Socials',
+        section: '社交媒体',
         handler: () => {
           window.open("https://bsky.app/profile/haoruiii.bsky.social", "_blank");
         },
       },{
         id: 'social-linkedin',
         title: 'LinkedIn',
-        section: 'Socials',
+        section: '社交媒体',
         handler: () => {
           window.open("https://www.linkedin.com/in/haoruiii", "_blank");
         },
       },{
         id: 'social-red_note',
         title: 'Red_note',
-        section: 'Socials',
+        section: '社交媒体',
         handler: () => {
           window.open("https://www.xiaohongshu.com/user/profile/61309a24000000000201b7dd", "_blank");
         },
       },{
         id: 'social-bilibili_url',
         title: 'Bilibili_url',
-        section: 'Socials',
+        section: '社交媒体',
         handler: () => {
           window.open("", "_blank");
         },
@@ -548,8 +196,8 @@ ninja.data = [{
         },{
       id: 'light-theme',
       title: '浅色模式',
-      description: '将页面切换至浅色模式',
-      section: 'Theme',
+      description: '将页面主题切换至浅色',
+      section: '页面主题',
       handler: () => {
         setThemeSetting("light");
       },
@@ -557,17 +205,17 @@ ninja.data = [{
     {
       id: 'dark-theme',
       title: '深色模式',
-      description: '将页面切换至深色模式',
-      section: 'Theme',
+      description: '将页面主题切换至深色',
+      section: '页面主题',
       handler: () => {
         setThemeSetting("dark");
       },
     },
     {
       id: 'system-theme',
-      title: 'Use system default theme',
-      description: 'Change the theme of the site to System Default',
-      section: 'Theme',
+      title: '系统默认',
+      description: '将页面主题切换至系统默认',
+      section: '页面主题',
       handler: () => {
         setThemeSetting("system");
       },
